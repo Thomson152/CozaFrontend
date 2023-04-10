4f8402df09923756c5b1f003753df2ac98247859
Thomson152 <tomsinonyedikachi@gmail.com>
2023-04-10 23:35:19 +0100

    video added

--- src/Screens/ProfileScreen.jsx
+++ src/Screens/ProfileScreen.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React, { useState } from "react";
-
+import VideoBg from "../images/Ecommerce.mp4"
 import logo from "../images/logo-01.png";
 import { Link } from "react-router-dom";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
@@ -10,9 +10,9 @@ const ProfileScreen = () => {
 
   const [open, setOpen] = useState(false);
   return (
-    <div className="">
-      <div className="md:w-[30%] ">
-        <div className="absolute top-5  left-4 text-white cursor-pointer ">
+    <div className="flex md:flex-row flex-col relative w-[100%]">
+      <div className="md:w-[26%] w-[100%]  ">
+        <div className="absolute top-5 left-4 text-white cursor-pointer ">
           <HiMenuAlt3
             onClick={() => setOpen(!open)}
             className="bg-slate-900 text-4xl px-2 rounded-md"
@@ -20,7 +20,7 @@ const ProfileScreen = () => {
         </div>
 
         <div
-          className={`w-[300px]  fixed duration-500 top-0 bottom-0 bg-slate-800 overflow-y-auto h-screen  ${
+          className={`w-[300px]  fixed duration-500 top-0 bottom-0 bg-slate-800 overflow-y-auto ${
             open
               ? "left-[0px] transition-all  ease-in "
               : "left-[-300px] md:left-[0px] "
@@ -66,8 +66,11 @@ const ProfileScreen = () => {
         </div>
       </div>
 
-      <div className="md:w-[70%]">
-        <h1>hello</h1>
+      <div className="md:w-[70%]  w-[100%] ">
+        <div className="flex justify-center items-center  h-screen">
+        <video className="w-[100%] h-[100%] object-fit:cover" src={VideoBg} autoPlay loop muted/>
+        </div>
+       
       </div>
     </div>
   );
