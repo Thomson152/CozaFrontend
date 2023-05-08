c50d8cff9bf17b19ff5b66ad38f35d257c1c5c3f
Thomson152 <tomsinonyedikachi@gmail.com>
2023-05-08 09:39:30 +0100

    changes

--- src/components/CheckoutSteps.jsx
+++ src/components/CheckoutSteps.jsx
@@ -9,10 +9,16 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <ul className="md:px-4 px-2">
         {step1 ? (
           <li>
-            <Link to="/login" className="text-[18px]">Sign In</Link>
+            <Link to="/login" className=" md:text-[18px] text-[16px]">
+              Sign In
+            </Link>
           </li>
         ) : (
-          <Link disabled className="pointer-events-none text-gray-300" to="/login">
+          <Link
+            disabled
+            className="pointer-events-none text-gray-300  md:text-[18px] text-[16px]"
+            to="/login"
+          >
             Sign In
           </Link>
         )}
@@ -21,10 +27,16 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <ul className="md:px-4 px-2">
         {step2 ? (
           <li>
-            <Link to="/shipping" className="text-[18px]" >Shipping</Link>
+            <Link to="/shipping" className=" md:text-[18px] text-[16px]">
+              Shipping
+            </Link>
           </li>
         ) : (
-          <Link disabled className="pointer-events-none text-gray-300" to="/shipping">
+          <Link
+            disabled
+            className="pointer-events-none text-gray-300  md:text-[18px] text-[16px]"
+            to="/shipping"
+          >
             Shipping
           </Link>
         )}
@@ -33,10 +45,16 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <ul className="md:px-4 px-2">
         {step3 ? (
           <li>
-            <Link to="/payment" className="text-[18px]">Payment</Link>
+            <Link to="/payment" className=" md:text-[18px] text-[16px]">
+              Payment
+            </Link>
           </li>
         ) : (
-          <Link disabled className="pointer-events-none text-gray-300 text-[18px]" to="/payment">
+          <Link
+            disabled
+            className="pointer-events-none text-gray-300 md:text-[18px] text-[16px]"
+            to="/payment"
+          >
             Payment
           </Link>
         )}
@@ -45,10 +63,16 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <ul className="md:px-4 px-2">
         {step4 ? (
           <li>
-            <Link to="/placeorder" className="text-[18px]">Place Order</Link>
+            <Link to="/placeorder" className="text-[18px]">
+              Place Order
+            </Link>
           </li>
         ) : (
-          <Link disabled className="pointer-events-none text-gray-300 text-[18px]" to="/placeorder" >
+          <Link
+            disabled
+            className="pointer-events-none text-gray-300 text-[18px]"
+            to="/placeorder"
+          >
             Place Order
           </Link>
         )}
